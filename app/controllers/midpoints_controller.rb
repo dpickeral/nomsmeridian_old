bf39b72504773523da2a8a6bd07cd1b05c7b14f2
David Pickeral <dwpickeral@gmail.com>
2015-12-07 17:20:26 -0500

    yelp works

--- app/controllers/midpoints_controller.rb
+++ app/controllers/midpoints_controller.rb
@@ -10,6 +10,20 @@ class MidpointsController < ApplicationController
     address1 = Address.create(address: params[:address1])
     address2 = Address.create(address: params[:address2])
     midpoint = Geocoder::Calculations.geographic_center([address1.coordinates, address2.coordinates])
-    render text: "midpoint of #{address1.address} and #{address2.address} = #{midpoint}"
+    coordinates = { latitude: midpoint[0].round(4), longitude: midpoint[1].round(4) }
+    params = { terms: 'restaurant' , limit: 5 }
+    #render json: Yelp.client.search_by_coordinates(coordinates, params, locale)
+    #def search
+    #parameters = { term: params[:term], limit: 16 }
+    #render json: Yelp.client.search('San Francisco', parameters)
+    #end
+    #render text: "#{coordinates}"
+    #render text: "midpoint of #{address1.address} and #{address2.address} = #{midpoint}"
+    render json: Yelp.client.search_by_coordinates(coordinates, params)
+    
+
   end
+end
+
+def show
 end
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'Yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = '1YXJ58DmRxGiM5cuvmHIww'
+  config.consumer_secret = 'uQcUvlbIFo5rMB-aaMLGC92rquI'
+  config.token = '6KRh3MptscI5iB9NZ1cZhUo_rLbpwRx6'
+  config.token_secret = 'N7oSK6Ykc-rvcKewStV-fJowLwk'
+end
